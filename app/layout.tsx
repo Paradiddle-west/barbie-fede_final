65b64e45e0a384614ca7638b8894ee49bd656b24
Fede Caroni <federicoc@unitech.com.ar>
2025-04-09 20:56:35 -0300

    ¡nos casamos!

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Barbie & Fede | Nos casamos! 01/11/2025',
+  title: 'Barbie & Fede | ¡Nos casamos! 01/11/2025',
   description: 'El gran día es el 1 de noviembre de 2025 en Álamos de Cañuelas. Te esperamos!',
   generator: 'v0.dev',
   icons: {
